05b870584e35f3f940cb83a7d696b5dda411e12d
Hernan Rinaldi <hernanjavierrinaldi@gmail.com>
2024-03-09 18:13:09 -0300

    Add Activities

---
+++ Hernan_Rinaldi/app/src/main/java/com/hernanrinaldi/hernan_rinaldi/AboutActivity.kt
@@ -0,0 +1,11 @@
+package com.hernanrinaldi.hernan_rinaldi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
