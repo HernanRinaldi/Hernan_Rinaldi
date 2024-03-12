863fbd50cc79cfe96a4f89a8acaa0d242f2405a4
Hernan Rinaldi <hernanjavierrinaldi@gmail.com>
2024-03-11 22:45:17 -0300

    Add references for buttons

--- Hernan_Rinaldi/app/src/main/java/com/hernanrinaldi/hernan_rinaldi/MenuActivity.kt
+++ Hernan_Rinaldi/app/src/main/java/com/hernanrinaldi/hernan_rinaldi/MenuActivity.kt
@@ -1,11 +1,19 @@
 package com.hernanrinaldi.hernan_rinaldi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        val ButtonAbout = findViewById<Button>(R.id.About)
+        val ButtonSkills = findViewById<Button>(R.id.Skills)
+        val ButtonProjects = findViewById<Button>(R.id.Projects)
+        val ButtonContact = findViewById<Button>(R.id.Contact)
+
     }
 }
