e4d1319e1d329fc8989e91bf274f1c2b63767a8f
Hernan Rinaldi <hernanjavierrinaldi@gmail.com>
2024-03-13 17:52:28 -0300

    Add navigate back from projects and contact

--- Hernan_Rinaldi/app/src/main/java/com/hernanrinaldi/hernan_rinaldi/ContactActivity.kt
+++ Hernan_Rinaldi/app/src/main/java/com/hernanrinaldi/hernan_rinaldi/ContactActivity.kt
@@ -1,11 +1,22 @@
 package com.hernanrinaldi.hernan_rinaldi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ContactActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact)
+
+        val buttonBackMenu = findViewById<Button>(R.id.BtnBackMenu)
+        buttonBackMenu.setOnClickListener { navigateToMenu() }
+
     }
+    private fun navigateToMenu(){
+        val intentNavigateBack = Intent(this, MenuActivity::class.java)
+        startActivity(intentNavigateBack)
+    }
+
 }
