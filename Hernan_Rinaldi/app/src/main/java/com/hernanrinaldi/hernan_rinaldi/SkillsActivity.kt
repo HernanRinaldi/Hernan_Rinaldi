2ed8ca49356d030809c88e1605d9d9d8ed190ec7
Hernan Rinaldi <hernanjavierrinaldi@gmail.com>
2024-03-13 09:04:03 -0300

    Add button for navigate back to menu

--- Hernan_Rinaldi/app/src/main/java/com/hernanrinaldi/hernan_rinaldi/SkillsActivity.kt
+++ Hernan_Rinaldi/app/src/main/java/com/hernanrinaldi/hernan_rinaldi/SkillsActivity.kt
@@ -1,11 +1,22 @@
 package com.hernanrinaldi.hernan_rinaldi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SkillsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skills)
+
+        val btnBackMenu = findViewById<Button>(R.id.BtnBackMenu)
+        btnBackMenu.setOnClickListener { navigateToMenu() }
+
+    }
+
+     private fun navigateToMenu(){
+        val intentNavigateBack = Intent(this, MenuActivity::class.java)
+         startActivity(intentNavigateBack)
     }
 }
